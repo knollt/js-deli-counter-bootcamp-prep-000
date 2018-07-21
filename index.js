10812b8e60e34403bf5c9b2b37e3904e215d4200
timothy.knoll@gmail.com <timothy.knoll@gmail.com>
2018-07-21 01:30:38 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+let katzDeli = []
+
+function takeANumber (katzDeli, name){
+  // accept current line of people + personName
+  // return position in line
+  katzDeli.push(name)
+  
+  return "Welcome, " + name + ". You are number " + katzDeli.length + " in line."
+  
+}
+
+function nowServing (line){
+  // return first person etc
+  // nobody = "There is nobody to be served"
+  if(line.length === 0){
+    return "There is nobody waiting to be served!"
+  } else {
+    let name = line[0]
+    line.splice(0,1)
+    return "Currently serving " + name + "."
+  }
+}
+
+function currentLine (line) {
+  // returns line position
+  // empty = "The line is currently empty"
+  let lineNumber = []
+  if (line.length === 0){
+    return "The line is currently empty."
+  } else {
+    for (let i = 0; i < line.length; i++){
+      lineNumber += (i + 1) + ". " + line[i]
+    }
+    finalLine = lineNumber.join(", ")
+    return "The line is currently: " + finalLine
+  }  
+}
